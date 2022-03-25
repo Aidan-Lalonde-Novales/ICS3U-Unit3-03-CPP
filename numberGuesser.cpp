3cc21c74a1f6d5bda5f3269d87ad1f796adb1d2d
Aidan-Lalonde-Novales <ec2-user@ip-172-31-31-108.ec2.internal>
2022-03-25 03:45:11 +0000

    fart

--- numberGuesser.cpp
+++ numberGuesser.cpp
@@ -1,17 +1,24 @@
 // Copyright (c) 2022 Aidan Lalonde-Novales All rights reserved
 //
 // Created by Aidan Lalonde-Novales
-// Created on 2022-03-07
+// Created on 2022-03-24
 // This is a program that lets the user guess a number
 // between 0-9 and tell them if they got it right.
 
 #include <iostream>
 #include <iomanip>
+#include <random>
 
 int main() {
     // This function gets a guess from a user and tells them
     // if they got the number between 0-9 correct
-    const float RANDOM = 1;
+
+    int randomNumber;
+
+    std::random_device rseed;
+    std::mt19937 rgen(rseed());  // mersenne_twister
+    std::uniform_int_distribution<int> idist(0, 9);  // between 0-9
+    randomNumber = idist(rgen);
 
     float guessedNumber;
 
@@ -20,12 +27,13 @@ int main() {
     std::cin >> guessedNumber;
 
     // Process & Output
-    if (guessedNumber == RANDOM) {
+    if (guessedNumber == randomNumber) {
         std::cout << "You Guessed Correctly!" << std::endl;
         std::cout << std::endl;
         std::cout << "Done." << std::endl;
     } else {
-        std::cout << "You Guessed Incorrectly." << std::endl;
+        std::cout << "You Guessed Incorrectly. The correct number was "
+        << randomNumber << "." << std::endl;
         std::cout << std::endl;
         std::cout << "Done." << std::endl;
     }
